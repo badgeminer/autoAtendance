6cc5b6e1a798132bb257e47c52383229ada8b206
badge hunt <64038298+badgeminer@users.noreply.github.com>
2022-09-27 20:41:07 +0000

    ver check

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import tkinter as tk
 from tkinter import ttk
 import json
-
-
+import requests
+ver = "0.0.1"
+with requests.get("")
 stud = json.load(open("usrs.json"))["usrs"]
 
 studLs = list(stud.values())
