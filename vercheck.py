31117fae959316da60f80ae658cf51ca0cddaff3
badgeminer <64038298+badgeminer@users.noreply.github.com>
2022-11-01 10:38:15 -0600

    Update vercheck.py

--- vercheck.py
+++ vercheck.py
@@ -3,7 +3,9 @@ import requests,logging,sys,json
 
 chunks = 100
 
-def check(branch,cver):  
+def check(branch,cver):
+  global br
+  br = branch
   try:
     with requests.get(f"https://raw.githubusercontent.com/badgeminer/autoAtendance/{branch}/Ver") as v:
         v.raise_for_status()
@@ -22,14 +24,16 @@ def upd():
     files = json.loads(r.text)["reqs"]
     r.close()
     for file in files:
-      f = open(file, 'wb')
+      f = open(file+"u", 'wb')
+      r = requests.get("https://raw.githubusercontent.com/badgeminer/autoAtendance/"+br+"/"+file)
+      r.raise_for_status()
       for chunk in r.iter_content(chunk_size=int(chunks)): 
         if chunk: # filter out keep-alive new chunks
               f.write(chunk)
-        f.close()
-        r.close()  
-      print('done, ready for restart', "green")
-      sys.exit()
+      f.close()
+      r.close()  
+    print('done, ready for restart', "green")
+    sys.exit()
   except Exception as e:
         logging.fatal(f"InstallError: {str(e)}, reinstall might be required")
         sys.exit(137707)
