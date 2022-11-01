b62ed26d360ee2272139d2b93b5d0a4c55d29d3c
badgeminer <64038298+badgeminer@users.noreply.github.com>
2022-11-01 09:49:37 -0600

    v0.0.9 (#34)
    * .gitignore
    
    * gitignore again
    
    * scrollbar
    
    * basics
    
    * release service
    
    * vercfgs
    
    * Create README.md
    
    * Delete README.md
    
    * code refractor
    
    * beter code documentations
    
    * 0.0.7-Fix
    
    * bugfix
    
    * V0.0.8-cfgs
    
    * V0.0.7 (#17)
    
    * Update Ver
    
    * Update Ver
    
    * Update main.py
    
    * Update Ver
    
    * Update Ver
    
    * Update Ver
    
    * Update Ver
    
    * Update Ver
    
    * Update Ver
    
    * Update Ver
    
    * rrrrrg
    
    * bugFix-0.0.7 (#18)
    
    * .gitignore
    
    * gitignore again
    
    * scrollbar
    
    * basics
    
    * release service
    
    * vercfgs
    
    * Create README.md
    
    * Delete README.md
    
    * code refractor
    
    * beter code documentations
    
    * 0.0.7-Fix
    
    * bugfix
    
    * configs  change
    
    * V0.0.8-classes (#27)
    
    * V0.0.7 (#17)
    
    * Update Ver
    
    * Update Ver
    
    * Update main.py
    
    * Update Ver
    
    * Update Ver
    
    * Update Ver
    
    * Update Ver
    
    * Update Ver
    
    * Update Ver
    
    * Update Ver
    
    * rrrrrg
    
    * bugFix-0.0.7 (#18)
    
    * .gitignore
    
    * gitignore again
    
    * scrollbar
    
    * basics
    
    * release service
    
    * vercfgs
    
    * Create README.md
    
    * Delete README.md
    
    * code refractor
    
    * beter code documentations
    
    * 0.0.7-Fix
    
    * bugfix
    
    * configs  change
    
    * multiclass
    
    * ver
    
    * Create SECURITY.md
    
    * Update SECURITY.md
    
    * Delete SECURITY.md
    
    * V0.0.9-reoginise (#33)
    
    * configs  change
    
    * multiclass
    
    * downlad fix
    
    * reorg
    
    * upd gitnore
    
    * changes

---
+++ vercheck.py
@@ -0,0 +1,29 @@
+import requests,logging,sys,json
+def check(branch,cver):  
+  try:
+    with requests.get(f"https://raw.githubusercontent.com/badgeminer/autoAtendance/{branch}/Ver") as v:
+        v.raise_for_status()
+        if cver != v.text.replace("\n",""):
+            return True
+        return False
+  except requests.HTTPError as e:
+      logging.error(f"cant retreive latest version {str(e)}")
+
+def upd(chunks):
+  try:
+    r = requests.get("https://raw.githubusercontent.com/badgeminer/autoAtendance/RELEASE/files.json")
+    r.raise_for_status()
+    files = json.loads(r.text)["reqs"]
+    r.close()
+    for file in files:
+      f = open(file, 'wb')
+      for chunk in r.iter_content(chunk_size=int(chunks)): 
+        if chunk: # filter out keep-alive new chunks
+              f.write(chunk)
+        f.close()
+        r.close()  
+      print('done, ready for restart', "green")
+      sys.exit()
+  except Exception as e:
+        logging.fatal(f"InstallError: {str(e)}, reinstall might be required")
+        sys.exit(137707)
