1ddccba3916ad9419810b93701fda6459a5b6d13
badgeminer <64038298+badgeminer@users.noreply.github.com>
2022-11-28 13:55:53 -0700

    V0.1.0 close prot, google sheets integration, dark theme (#39)
    * v0.0.9-patch A -> v0.1.0 (#36)
    
    * v0.0.8 (#28)
    
    * .gitignore
    
    * gitignore again
    
    * scrollbar
    
    * basics
    
    * release service
    
    * vercfgs
    
    * Create README.md
    
    * Delete README.md
    
    * code refractor
    
    * beter code documentations
    
    * 0.0.7-Fix
    
    * bugfix
    
    * V0.0.8-cfgs
    
    * V0.0.7 (#17)
    
    * Update Ver
    
    * Update Ver
    
    * Update main.py
    
    * Update Ver
    
    * Update Ver
    
    * Update Ver
    
    * Update Ver
    
    * Update Ver
    
    * Update Ver
    
    * Update Ver
    
    * rrrrrg
    
    * bugFix-0.0.7 (#18)
    
    * .gitignore
    
    * gitignore again
    
    * scrollbar
    
    * basics
    
    * release service
    
    * vercfgs
    
    * Create README.md
    
    * Delete README.md
    
    * code refractor
    
    * beter code documentations
    
    * 0.0.7-Fix
    
    * bugfix
    
    * configs  change
    
    * V0.0.8-classes (#27)
    
    * V0.0.7 (#17)
    
    * Update Ver
    
    * Update Ver
    
    * Update main.py
    
    * Update Ver
    
    * Update Ver
    
    * Update Ver
    
    * Update Ver
    
    * Update Ver
    
    * Update Ver
    
    * Update Ver
    
    * rrrrrg
    
    * bugFix-0.0.7 (#18)
    
    * .gitignore
    
    * gitignore again
    
    * scrollbar
    
    * basics
    
    * release service
    
    * vercfgs
    
    * Create README.md
    
    * Delete README.md
    
    * code refractor
    
    * beter code documentations
    
    * 0.0.7-Fix
    
    * bugfix
    
    * configs  change
    
    * multiclass
    
    * Update Ver
    
    * Update Ver
    
    * Update Ver
    
    * vers
    
    * Create files.json
    
    * Update files.json
    
    * V0.0.9-reoginise (#33)
    
    * configs  change
    
    * multiclass
    
    * downlad fix
    
    * reorg
    
    * upd gitnore
    
    * changes
    
    * v0.0.9 (#34)
    
    * .gitignore
    
    * gitignore again
    
    * scrollbar
    
    * basics
    
    * release service
    
    * vercfgs
    
    * Create README.md
    
    * Delete README.md
    
    * code refractor
    
    * beter code documentations
    
    * 0.0.7-Fix
    
    * bugfix
    
    * V0.0.8-cfgs
    
    * V0.0.7 (#17)
    
    * Update Ver
    
    * Update Ver
    
    * Update main.py
    
    * Update Ver
    
    * Update Ver
    
    * Update Ver
    
    * Update Ver
    
    * Update Ver
    
    * Update Ver
    
    * Update Ver
    
    * rrrrrg
    
    * bugFix-0.0.7 (#18)
    
    * .gitignore
    
    * gitignore again
    
    * scrollbar
    
    * basics
    
    * release service
    
    * vercfgs
    
    * Create README.md
    
    * Delete README.md
    
    * code refractor
    
    * beter code documentations
    
    * 0.0.7-Fix
    
    * bugfix
    
    * configs  change
    
    * V0.0.8-classes (#27)
    
    * V0.0.7 (#17)
    
    * Update Ver
    
    * Update Ver
    
    * Update main.py
    
    * Update Ver
    
    * Update Ver
    
    * Update Ver
    
    * Update Ver
    
    * Update Ver
    
    * Update Ver
    
    * Update Ver
    
    * rrrrrg
    
    * bugFix-0.0.7 (#18)
    
    * .gitignore
    
    * gitignore again
    
    * scrollbar
    
    * basics
    
    * release service
    
    * vercfgs
    
    * Create README.md
    
    * Delete README.md
    
    * code refractor
    
    * beter code documentations
    
    * 0.0.7-Fix
    
    * bugfix
    
    * configs  change
    
    * multiclass
    
    * ver
    
    * Create SECURITY.md
    
    * Update SECURITY.md
    
    * Delete SECURITY.md
    
    * V0.0.9-reoginise (#33)
    
    * configs  change
    
    * multiclass
    
    * downlad fix
    
    * reorg
    
    * upd gitnore
    
    * changes
    
    * Update files.json
    
    * chunks
    
    * main
    
    * patch
    
    * updater change
    
    * patched
    
    * dark theme
    
    * ajustments
    
    * fix rqs and gitignore
    
    * Delete latest.log
    
    * Update .gitignore
    
    making shure that keys are not shared with the public
    
    * Update gs.py
    
    * finish google sheet integration
    
    finish google sheet integration and add code documentation in gs.py and vercheck.py
    
    * Update main.py
    
    * close protection

--- gs.py
+++ gs.py
@@ -1,11 +1,24 @@
-from __future__ import print_function
+# Auto Attendance
+# google sheets intergration library
 
+# imports
+from __future__ import print_function 
 
-from googleapiclient.discovery import build
+import datetime
+import logging
 
 from google.oauth2 import service_account
+from googleapiclient.discovery import build
 
+#define functions
+
+#init
 def init(id):
+  """sets up the google sheets api connection
+
+    Args:
+        id (spreadsheet id): used to pick google sheet
+  """
   global SPREADSHEET_ID,SCOPES,SERVICE_ACCOUNT_FILE,credentials
   SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
   SERVICE_ACCOUNT_FILE = 'keys.json'
@@ -15,7 +28,7 @@ def init(id):
         SERVICE_ACCOUNT_FILE, scopes=SCOPES)
   SPREADSHEET_ID = id
 
-
+#UNUSED: read sheet
 def read():
     service = build('sheets', 'v4', credentials=credentials)
 
@@ -30,27 +43,48 @@ def read():
     else:
         return values
 
-def write():
+#write to sheet
+def write(built:dict):
+    """appends built data to google sheet
+
+    Args:
+        built (dict): built google sheet data from build_sheet()
+    """
     service = build('sheets', 'v4', credentials=credentials)
 
     # Call the Sheets API
     sheet = service.spreadsheets()
     request = sheet.values().append(
         spreadsheetId=SPREADSHEET_ID,
-        range="data!a2",
+        range="data!A2:AE",
         valueInputOption="USER_ENTERED",
         insertDataOption="INSERT_ROWS",
-        body={
-            "values":[
-                ["e"]
-            ]
-        }
+        body=built
     )
-    response = request.execute()
+    try:
+        response = request.execute()
+    except BaseException as e:
+        logging.critical(str(e))
+
+#build sheet row and write
+def build_sheet(studs: dict()):
+    """builds data into google sheet format and writes it
+
+    Args:
+        studs (dict): student data here / not here data from auto attendance
+    """
+    body={
+            "values":[
+                [str(datetime.date.today()),],
+            ],
+        }
+    stds = list(studs.keys())
+    stds.sort()
+    for s in stds:
+        body["values"][0].append(str(studs[s]))
+    write(body)
 
 
-
-
+#UNUSED: tests
 if __name__ == '__main__':
-    #read()
-    write()
+    pass
--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 #    By Badgeminer2    #
 # a open sorce project #
 ########################
-
+#greifed: 1 time
 ###################
 # ver = version   #
 # stud = students #
@@ -14,6 +14,7 @@
 
 #imports 
 import tkinter as tk
+from tkinter import ttk
 import json, sys
 import colorama
 from termcolor import colored
@@ -22,8 +23,13 @@ import getopt
 import logging
 import coloredlogs
 import vercheck as updr
+from tkinter.simpledialog import askstring
+#from ctypes import windll
+import platform
 
 import gs
+
+
 
 
 #inits
@@ -42,7 +48,7 @@ ver = config['DEFAULT']['version']
 
 logging.debug("starting system\n\n\n\n")
 updr.chunks = int(config["DEFAULT"]['downloadChunkSize'])
-
+updr.branch = config["UPDATE"]['verBranch']
 if config["sheets"].getboolean('enabled'):
   gs.init(config["sheets"]["id"])
 
@@ -63,20 +69,134 @@ studLs = list(stud.values())
 studLs.sort()
 studs = dict.fromkeys(studLs, False)
 
+
 #cofigs
 
 
 dcs = int(config['DEFAULT']['downloadChunkSize'])
 max = int(config['ATTENDANCE']['maxStud'])
-
-
+sync = config['sheets'].getboolean('enabled')
+safetoclose = not config['DEFAULT'].getboolean('closeprot')
+cpsw = config['DEFAULT']['closepsw']
 
 #update checking
 if config['UPDATE'].getboolean('checkForUpd'):
     upd =updr.check(config["UPDATE"]["verBranch"],ver)
 
+#custom title bar
+
+# title bar colors
+TITLE_FOREGROUND = "white"
+TITLE_BACKGROUND = "#2c2c2c"
+TITLE_BACKGROUND_HOVER = "green"
+
+BUTTON_FOREGROUND = "white"
+BUTTON_BACKGROUND = TITLE_BACKGROUND
+BUTTON_FOREGROUND_HOVER = BUTTON_FOREGROUND
+BUTTON_min_FOREGROUND_HOVER = "white"
+BUTTON_min_BACKGROUND_HOVER = 'blue'
+BUTTON_BACKGROUND_HOVER = 'red'
+
+# window colors
+WINDOW_BACKGROUND = "white"
+WINDOW_FOREGROUND = "black"
+
+def close(*args,**kwargs):
+    if safetoclose:
+        root.destroy()
+    else:
+        psw = askstring("enter the pasword to close", "enter the pasword to close auto attendance")
+        if psw == cpsw:
+            root.destroy()
+        else:
+            tk.messagebox.showerror("invalid psw","the psw entered is invalid")
+
+
+def darkstyle(root):
+    ''' Return a dark style to the window'''
+    
+    style = ttk.Style(root)
+    return style
+
+class MyButton(tk.Button):
+
+    def __init__(self, master, text='x', command=None,afg=BUTTON_FOREGROUND_HOVER,abg=BUTTON_BACKGROUND_HOVER, **kwargs):
+        super().__init__(master, bd=0, font="bold", padx=5, pady=2, 
+                         fg=BUTTON_FOREGROUND, 
+                         bg=BUTTON_BACKGROUND,
+                         activebackground=abg,
+                         activeforeground=afg, 
+                         highlightthickness=0, 
+                         text=text,
+                         command=command)
+
+        self.bind('<Enter>', self.on_enter)
+        self.bind('<Leave>', self.on_leave)
+
+    def on_enter(self, event):
+        self['bg'] = BUTTON_BACKGROUND_HOVER
+
+    def on_leave(self, event):
+        self['bg'] = BUTTON_BACKGROUND
+
+class MyTitleBar(tk.Frame):
+
+    def __init__(self, master, *args, **kwargs):
+        super().__init__(master, relief='raised', bd=1, 
+                         bg=TITLE_BACKGROUND,
+                         highlightcolor=TITLE_BACKGROUND, 
+                         highlightthickness=0)
+        self.columnconfigure(4, weight=3)
+        self.title_label = tk.Label(self, 
+                                    bg=TITLE_BACKGROUND, 
+                                    fg=TITLE_FOREGROUND)
+                                    
+        self.set_title("Auto Attendance V"+ver)
+
+        self.close_button = MyButton(self, text='x', command=close)
+        #self.minimize_button = MyButton(self, text='-', command=self.on_minimize,afg=BUTTON_min_FOREGROUND_HOVER,abg=BUTTON_min_BACKGROUND_HOVER)
+        self.other_button = MyButton(self, text='?', command=self.on_other)
+                         
+        self.grid(column=0, row=0, sticky='ew',columnspan=4)
+        self.title_label.grid(column=0, row=0,columnspan=4)
+        self.close_button.grid(column=7, row=0,sticky='ew')
+        #self.minimize_button.grid(column=6, row=0,sticky='w')
+        self.other_button.grid(column=5, row=0,sticky="w")
+
+        self.bind("<ButtonPress-1>", self.on_press)
+        self.bind("<ButtonRelease-1>", self.on_release)
+        self.bind("<B1-Motion>", self.on_move)
+        
+    def set_title(self, title):
+        self.title = title
+        self.title_label['text'] = title
+        
+    def on_press(self, event):
+        self.xwin = event.x
+        self.ywin = event.y
+        self.set_title("Auto Attendance V"+ver+" - ... I'm moving! ...")
+        self['bg'] = 'green'
+        self.title_label['bg'] = TITLE_BACKGROUND_HOVER
+
+    def on_release(self, event):
+        self.set_title("Auto Attendance V"+ver)
+        self['bg'] = TITLE_BACKGROUND
+        self.title_label['bg'] = TITLE_BACKGROUND
+        
+    def on_move(self, event):
+        x = event.x_root - self.xwin
+        y = event.y_root - self.ywin
+        self.master.geometry(f'+{x}+{y}')
+        
+    def on_minimize(self):
+        print('TODO: minimize')
+    
+                
+    def on_other(self):
+        print('TODO: other')
+      
 #define the main window
-class window(tk.Frame):
+class window(ttk.Frame):
     
     #setup window
     def __init__(self, master):
@@ -85,7 +205,8 @@ class window(tk.Frame):
         #configure grid rows and columns
         self.rowconfigure(0, weight=2)
         self.rowconfigure(1, weight=2)
-        self.rowconfigure(4, weight=5)
+        self.rowconfigure(2, weight=2)
+        self.rowconfigure(5, weight=5)
         self.columnconfigure(1, weight=1)
 
         #if there is a update add the update btn
@@ -94,12 +215,12 @@ class window(tk.Frame):
                                  command=updr.upd,
                                  activebackground="red",
                                  background="red")
-            self.upd.grid(column=0, row=1, columnspan=4)
+            self.upd.grid(column=0, row=2, columnspan=4)
 
         #setup entry box
-        self.entrythingy = tk.Entry()
+        self.entrythingy = tk.Entry(foreground="white",background='#3d3d3d')
 
-        # tk vars
+        # ttk vars
         self.contents = tk.StringVar()
 
         #setup input box
@@ -108,23 +229,33 @@ class window(tk.Frame):
         self.entrythingy.bind('<Key-Return>', self.print_contents)
 
         #setup listboxes
-        self.notHere = tk.Listbox()
-        self.Here = tk.Listbox()
+        self.notHere = tk.Listbox(foreground="white",background='#3d3d3d')
+        self.Here = tk.Listbox(foreground="white",background='#3d3d3d')
+        #self.notHere = ttk.Treeview()
+        #self.Here = ttk.Treeview()
 
         #setup scroll bars
         self.scroll = tk.Scrollbar()
         self.scrollH = tk.Scrollbar()
       
         #setup labels
-        self.lableNH = tk.Label(text="Not Here")
-        self.lableH = tk.Label(text="Here")
-        self.resetB = tk.Button(text="reset", command=self.reset, activeforeground="red")
+        self.lableNH = tk.Label(text="Not Here",foreground="white",background='#3d3d3d')
+        self.lableH = tk.Label(text="Here",foreground="white",background='#3d3d3d')
+
+        #setup btns
+        #self.resetB = ttk.Button(text="reset", command=self.reset, activeforeground="red")
+        #self.syncb = ttk.Button(text="sync", command=self.sync, activeforeground="blue")
+        self.resetB = tk.Button(text="reset", command=self.reset,activeforeground="red",foreground="white",background='#3d3d3d',activebackground='#3d3d3d')
+        self.syncb = tk.Button(text="sync", command=self.sync,foreground="white",activeforeground="blue",background='#3d3d3d',activebackground='#3d3d3d')
 
         #add reset button to grid
-        self.resetB.grid(column=2, row=1)
+        self.resetB.grid(column=2, row=2)
+
+        #add sync
+        self.syncb.grid(column=0, row=2)
 
         #add entry box to grid
-        self.entrythingy.grid(column=0, row=0, columnspan=4)
+        self.entrythingy.grid(column=0, row=1, columnspan=4)
       
         #add studs to lists
         I = 0
@@ -133,16 +264,16 @@ class window(tk.Frame):
             I += 1
 
         #add labels to grid
-        self.lableNH.grid(column=0, row=2)
-        self.lableH.grid(column=2, row=2)
+        self.lableNH.grid(column=0, row=3)
+        self.lableH.grid(column=2, row=3)
 
         #add scroll bars to grid
-        self.scroll.grid(column=1, row=3, sticky="ns")
-        self.scrollH.grid(column=3, row=3, sticky="ns")
+        self.scroll.grid(column=1, row=4, sticky="ns")
+        self.scrollH.grid(column=3, row=4, sticky="ns")
 
         #add lists to grid
-        self.notHere.grid(column=0, row=3)
-        self.Here.grid(column=2, row=3)
+        self.notHere.grid(column=0, row=4)
+        self.Here.grid(column=2, row=4)
 
         #event bindings
         self.notHere.config(yscrollcommand=self.scroll.set)
@@ -164,7 +295,8 @@ class window(tk.Frame):
 
             self.Here.delete(entrys.index(place))
             self.notHere.insert(studLs.index(place), place)
-        except tk.TclError as e:
+            studs[place] = False
+        except ttk.TclError as e:
             logging.warning(f"usrMoveErr: {str(e)}")
 
     #move ppl to here
@@ -177,7 +309,8 @@ class window(tk.Frame):
 
             self.notHere.delete(entrys.index(place))
             self.Here.insert(studLs.index(place), place)
-        except tk.TclError as e:
+            studs[place] = True
+        except ttk.TclError as e:
             logging.warning(f"usrMoveErr: {str(e)}")
 
     #scan and move
@@ -191,6 +324,7 @@ class window(tk.Frame):
                 self.notHere.delete(entrys.index(stud[self.contents.get()]))
                 self.Here.insert(studLs.index(stud[self.contents.get()]),
                                  stud[self.contents.get()])
+                studs[stud[self.contents.get()]] = True
                 print(colored(f"{self.contents.get()}:{stud[self.contents.get()]} NotHere -> Here","green"))
             except Exception as e:
                 logging.warning(f"problem with moving student: {str(e)}")
@@ -210,11 +344,29 @@ class window(tk.Frame):
             logging.debug(f"moving {str(i)} to id_{str(I)}")
             self.notHere.insert(I, i)
             I += 1
+            studs[i] = False
+
+    def sync(self):
+      if sync:
+        gs.build_sheet(studs)
+      
+
+
+
 
 
 
 #run the app
 root = tk.Tk()
+style = darkstyle(root)
+root.overrideredirect(True)
+root.attributes("-topmost", True)
+title_bar = MyTitleBar(root) 
+if platform.system() == "Windows":
+  root.geometry('285x260')
+else:
+  root.geometry('355x260')
+root.configure(background='#3d3d3d')
 myapp = window(root)
 myapp.master.title(f"Auto Attendance V{ver}")
 try:
